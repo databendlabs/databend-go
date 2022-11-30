da7cc5ef8c9822db8483a8c6ede24257bf69986c
everpcpc <git@everpcpc.com>
2022-11-30 15:35:30 +0800

    feat: support tenant in dsn args

--- restful_test.go
+++ restful_test.go
@@ -8,13 +8,14 @@ import (
 
 func TestMakeHeaders(t *testing.T) {
 	c := APIClient{
-		User:     "root",
-		Password: "root",
-		Host:     "tn3ftqihs--bl.ch.aws-us-east-2.default.databend.com",
+		User:      "root",
+		Password:  "root",
+		Host:      "tn3ftqihs--bl.ch.aws-us-east-2.default.databend.com",
+		Tenant:    "tn3ftqihs",
+		Warehouse: "bl",
 	}
 	headers := c.makeHeaders()
 	assert.Equal(t, headers["Authorization"], []string{"Basic cm9vdDpyb290"})
-	// assert.Equal(t, headers["X-Databend-Tenant"], []string{"tn3ftqihs"})
-	// assert.Equal(t, headers["X-Databend-Warehouse"], []string{"bl"})
-
+	assert.Equal(t, headers["X-Databend-Tenant"], []string{"tn3ftqihs"})
+	assert.Equal(t, headers["X-Databend-Warehouse"], []string{"bl"})
 }
