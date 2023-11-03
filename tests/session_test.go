c0bd5805d600c5a878ce2cdc777860a00dfcf17b
zhihanz <zzh243402448@gmail.com>
2023-11-03 10:51:13 +0800

    chore: debug

--- tests/session_test.go
+++ tests/session_test.go
@@ -1,6 +1,11 @@
 package tests
 
-import "github.com/stretchr/testify/require"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"os"
+)
 
 func (s *DatabendTestSuite) TestChangeDatabase() {
 	r := require.New(s.T())
@@ -27,20 +32,27 @@ func (s *DatabendTestSuite) TestChangeRole() {
 	println(result)
 	_, err = s.db.Exec("create role if not exists test_role")
 	r.Nil(err)
-	defer s.db.Exec("drop role if exists test_role")
-	_, err = s.db.Exec("set role 'test_role'")
-	r.Nil(err)
+	dsn := os.Getenv("TEST_DATABEND_DSN")
+	s.NotEmpty(dsn)
+	dsn = fmt.Sprintf("%s?role=test_role", dsn)
+	s.db, err = sql.Open("databend", dsn)
+	s.Nil(err)
 	err = s.db.QueryRow("select current_role()").Scan(&result)
 	r.Nil(err)
 	r.Equal("test_role", result)
-	_, err = s.db.Exec("create role if not exists test_role_2")
-	r.Nil(err)
-	defer s.db.Exec("drop role if exists test_role_2")
-	_, err = s.db.Exec("set role 'test_role_2'")
-	r.Nil(err)
-	err = s.db.QueryRow("select current_role()").Scan(&result)
-	r.Nil(err)
-	r.Equal("test_role_2", result)
+	//
+	//defer s.db.Exec("drop role if exists test_role")
+	//_, err = s.db.Exec("set role 'test_role'")
+	//r.Nil(err)
+	//
+	//_, err = s.db.Exec("create role if not exists test_role_2")
+	//r.Nil(err)
+	//defer s.db.Exec("drop role if exists test_role_2")
+	//_, err = s.db.Exec("set role 'test_role_2'")
+	//r.Nil(err)
+	//err = s.db.QueryRow("select current_role()").Scan(&result)
+	//r.Nil(err)
+	//r.Equal("test_role_2", result)
 }
 
 func (s *DatabendTestSuite) TestSessionConfig() {
