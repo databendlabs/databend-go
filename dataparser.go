5537a780aa750d26684958cb282f62e2c268694c
everpcpc <git@everpcpc.com>
2024-08-01 16:35:22 +0800

    z

--- dataparser.go
+++ dataparser.go
@@ -686,8 +686,6 @@ func newDataParser(t *TypeDesc, unquote bool, opt *DataParserOptions) (DataParse
 			return nil, err
 		}
 		return &nullableParser{innerParser: inner, innerType: t.Args[0].Name}, nil
-	case "NULL":
-		return &nothingParser{}, nil
 	case "Date":
 		loc := time.UTC
 		if opt != nil && opt.Location != nil {
