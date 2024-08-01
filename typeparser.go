40a7aaea1fa8083bad5166c2b2c3740548a73e56
everpcpc <git@everpcpc.com>
2024-08-01 12:17:08 +0800

    z

--- typeparser.go
+++ typeparser.go
@@ -1,96 +1,151 @@
 package godatabend
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // TypeDesc describes a (possibly nested) data type returned by Databend.
 type TypeDesc struct {
-	Name string
-	Args []*TypeDesc
+	Name     string
+	Nullable bool
+	Args     []*TypeDesc
 }
 
-func parseTypeDesc(tokens []*token) (*TypeDesc, []*token, error) {
-	var name string
-	if tokens[0].kind == 's' || tokens[0].kind == 'q' {
-		name = tokens[0].data
-		tokens = tokens[1:]
-	} else {
-		return nil, nil, fmt.Errorf("failed to parse type name: wrong token type '%c'", tokens[0].kind)
-	}
+// fn parse_type_desc(s: &str) -> Result<TypeDesc> {
+//     let mut name = "";
+//     let mut args = vec![];
+//     let mut depth = 0;
+//     let mut start = 0;
+//     let mut nullable = false;
+//     for (i, c) in s.chars().enumerate() {
+//         match c {
+//             '(' => {
+//                 if depth == 0 {
+//                     name = &s[start..i];
+//                     start = i + 1;
+//                 }
+//                 depth += 1;
+//             }
+//             ')' => {
+//                 depth -= 1;
+//                 if depth == 0 {
+//                     let s = &s[start..i];
+//                     if !s.is_empty() {
+//                         args.push(parse_type_desc(s)?);
+//                     }
+//                     start = i + 1;
+//                 }
+//             }
+//             ',' => {
+//                 if depth == 1 {
+//                     let s = &s[start..i];
+//                     args.push(parse_type_desc(s)?);
+//                     start = i + 1;
+//                 }
+//             }
+//             ' ' => {
+//                 if depth == 0 {
+//                     let s = &s[start..i];
+//                     if !s.is_empty() {
+//                         name = s;
+//                     }
+//                     start = i + 1;
+//                 }
+//             }
+//             _ => {}
+//         }
+//     }
+//     if depth != 0 {
+//         return Err(Error::Parsing(format!("Invalid type desc: {}", s)));
+//     }
+//     if start < s.len() {
+//         let s = &s[start..];
+//         if !s.is_empty() {
+//             if name.is_empty() {
+//                 name = s;
+//             } else if s == "NULL" {
+//                 nullable = true;
+//             } else {
+//                 return Err(Error::Parsing(format!(
+//                     "Invalid type arg for {}: {}",
+//                     name, s
+//                 )));
+//             }
+//         }
+//     }
+//     Ok(TypeDesc {
+//         name,
+//         nullable,
+//         args,
+//     })
+// }
 
-	desc := TypeDesc{Name: name}
-	if tokens[0].kind != '(' {
-		return &desc, tokens, nil
-	}
+// convert rust to golang
 
-	tokens = tokens[1:]
+func ParseTypeDesc(s string) (*TypeDesc, error) {
+	var (
+		name     = ""
+		args     = []*TypeDesc{}
+		depth    = 0
+		start    = 0
+		nullable = false
+	)
 
-	if tokens[0].kind == ')' {
-		return &desc, tokens[1:], nil
-	}
-
-	if name == "Enum8" || name == "Enum16" {
-		// TODO: an Enum's arguments get completely ignored
-		for i := range tokens {
-			if tokens[i].kind == ')' {
-				return &desc, tokens[i+1:], nil
+	for i, c := range s {
+		switch c {
+		case '(':
+			if depth == 0 {
+				name = s[start:i]
+				start = i + 1
+			}
+			depth++
+		case ')':
+			depth--
+			if depth == 0 {
+				s := s[start:i]
+				if s != "" {
+					desc, err := ParseTypeDesc(s)
+					if err != nil {
+						return nil, err
+					}
+					args = append(args, desc)
+				}
+				start = i + 1
+			}
+		case ',':
+			if depth == 1 {
+				s := s[start:i]
+				if s != "" {
+					desc, err := ParseTypeDesc(s)
+					if err != nil {
+						return nil, err
+					}
+					args = append(args, desc)
+				}
+				start = i + 1
+			}
+		case ' ':
+			if depth == 0 {
+				s := s[start:i]
+				if s != "" {
+					name = s
+				}
+				start = i + 1
 			}
 		}
-		return nil, nil, fmt.Errorf("unfinished enum type description")
 	}
-
-	for {
-		var arg *TypeDesc
-		var err error
-
-		arg, tokens, err = parseTypeDesc(tokens)
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to parse subtype: %v", err)
-		}
-		desc.Args = append(desc.Args, arg)
-
-		switch tokens[0].kind {
-		case ',':
-			tokens = tokens[1:]
-			continue
-		case ')':
-			return &desc, tokens[1:], nil
+	if depth != 0 {
+		return nil, fmt.Errorf("invalid type desc: %s", s)
+	}
+	if start < len(s) {
+		s := s[start:]
+		if s != "" {
+			if name == "" {
+				name = s
+			} else if s == "NULL" {
+				nullable = true
+			} else {
+				return nil, fmt.Errorf("invalid type arg for %s: %s", name, s)
+			}
 		}
 	}
-}
-
-// ParseTypeDesc parses the type description that Databend  provides.
-//
-// The grammar is quite simple:
-//
-//	desc
-//	    name
-//	    name()
-//	    name(args)
-//	args
-//	    desc
-//	    desc, args
-//
-// Examples:
-//
-//	String
-//	Nullable(Nothing)
-//	Array(Tuple(Tuple(String, String), Tuple(String, UInt64)))
-func ParseTypeDesc(s string) (*TypeDesc, error) {
-	tokens, err := tokenizeString(s)
-	if err != nil {
-		return nil, fmt.Errorf("failed to tokenize type description: %v", err)
-	}
-
-	desc, tail, err := parseTypeDesc(tokens)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse type description: %v", err)
-	}
-
-	if len(tail) != 1 || tail[0].kind != eof {
-		return nil, fmt.Errorf("unexpected tail after type description")
-	}
-
-	return desc, nil
+	return &TypeDesc{Name: name, Nullable: nullable, Args: args}, nil
 }
