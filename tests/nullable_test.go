d9a20b81cdce2d1e198db7b23e66c8947bc21b94
everpcpc <git@everpcpc.com>
2024-08-06 16:47:47 +0800

    z

--- tests/nullable_test.go
+++ tests/nullable_test.go
@@ -13,16 +13,16 @@ func (s *DatabendTestSuite) TestNullable() {
 	s.r.Equal([][]interface{}{{int64(1), nil, nil, "NULL", "NULL", nil, nil, nil, nil}}, result)
 	s.r.NoError(rows.Close())
 
-	tx, err := s.db.Begin()
+	_, err = s.db.Exec("SET GLOBAL format_null_as_str=0")
 	s.r.Nil(err)
-	_, err = s.db.Exec("SET format_null_as_str=0")
-	s.r.Nil(err)
-	rows, err = tx.Query(fmt.Sprintf("SELECT * FROM %s", s.table))
+
+	rows, err = s.db.Query(fmt.Sprintf("SELECT * FROM %s", s.table))
 	s.r.Nil(err)
 	result, err = scanValues(rows)
 	s.r.Nil(err)
 	s.r.Equal([][]interface{}{{int64(1), nil, nil, nil, nil, nil, nil, nil, nil}}, result)
 	s.r.NoError(rows.Close())
-	err = tx.Rollback()
+
+	_, err = s.db.Exec("UNSET format_null_as_str")
 	s.r.Nil(err)
 }
